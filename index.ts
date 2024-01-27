a1f56b6e806ec6db9d62b108b60f6937afd8b435
Eroma-EA <ergazi.avdanov.ea@gmail.com>
2024-01-27 10:28:39 +0600

    use router

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import * as dotenv from "dotenv";
+import database from "./config/database";
+import router from "./router";
+dotenv.config();
+
+
+const port=process.env.PORT
+const app = express()
+app.use("/api", router)
+
+database.connectToDatabase()
+    .then(() => {
+        console.log("Database connection successfully.");
+
+        app.listen(port, () => {
+            console.log(`[server]:🧙🏻‍♂️ Server is running at http://localhost:${port}`);
+        });
+    })
+    .catch(err => {
+        console.error("Failed to connect to the database:", err);
+        process.exit(1);
+    });
